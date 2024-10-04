b21b2f7f7a71edbb6d0ddae3dde252eb8241ae80
umersheikh1986 <142933742+umersheikh1986@users.noreply.github.com>
2024-10-04 15:55:44 +0500

    chnages made

--- Gentop-main/Gentop/src/app/(marketing)/page.tsx
+++ Gentop-main/Gentop/src/app/(marketing)/page.tsx
@@ -6,19 +6,6 @@ import Decentralization from "@/components/marketing/Decentralization";
 import CombinedLevel from "@/components/marketing/combinedlevel";
 import IndividualLevel from "@/components/marketing/individuallevel";
 import Registration from "@/components/marketing/registration";
-import Upgrade from "@/components/marketing/upgrade";
-import OverPass from "@/components/marketing/overpass";
-import Distribution from "@/components/marketing/distribution";
-import Matrix from "@/components/marketing/matrix";
-import Recycle from "@/components/marketing/recycle";
-import EarnMatrix from "@/components/marketing/earnmatrix";
-import Table from "@/components/marketing/table";
-import Matriz from "@/components/marketing/matriz";
-import RecycleG3 from "@/components/marketing/recycleg3";
-import Table2 from "@/components/marketing/table2";
-import Start from "@/components/marketing/start";
-import Geniosfooter from "@/components/marketing/geniosfooter";
-import Faq from "@/components/marketing/faq";
 
 export default function Home() {
   return (
@@ -31,19 +18,6 @@ export default function Home() {
       <CombinedLevel />
       <IndividualLevel />
       <Registration />
-      {/* <Upgrade /> */}
-      {/* <OverPass /> */}
-      {/* <Distribution /> */}
-      {/* <Matrix /> */}
-      {/* <Recycle /> */}
-      {/* <EarnMatrix /> */}
-      {/* <Table /> */}
-      {/* <Matriz /> */}
-      {/* <RecycleG3 /> */}
-      {/* <Table2 /> */}
-      {/* <Start /> */}
-      {/* <Faq /> */}
-      {/* <Geniosfooter /> */}
     </main>
   );
 }
