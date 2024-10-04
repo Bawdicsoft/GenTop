2a5088ea142406a533f715fcd3ec116ee365571b
umersheikh1986 <142933742+umersheikh1986@users.noreply.github.com>
2024-10-04 17:18:42 +0500

    image updated

--- Gentop-main/Gentop/src/components/marketing/HeroSection.tsx
+++ Gentop-main/Gentop/src/components/marketing/HeroSection.tsx
@@ -1,35 +1,38 @@
 import Image from "next/image";
 function HeroSection() {
   return (
-    <div className="grid grid-rows-1 justify-center items-center   bg-[url('/backgroundimage.png')]">
+    <div className="grid grid-rows-1 justify-center items-center pb-10 pt-40  bg-[url('/backgroundimage.png')]">
       <section>
-        <div className="relative  z-30 h-[500px] w-[550px] mx-auto mt-40 mb-20 flex items-center justify-center">
-          {/* Base Image */}
-          <Image
-            src="/Gft-1.png"
-            width={550}
-            height={550}
-            alt="gtf-img"
-            className="absolute mt-6   "
-          />
+        <div className="border-2 border-b-white rounded-sm">
+          <div className="relative  z-30 h-[250px] w-[550px] mx-auto mt-40 mb-20 flex items-center justify-center">
+            {/* Base Image */}
+            <Image
+              src="/Gft-1.png"
+              width={550}
+              height={550}
+              alt="gtf-img"
+              className="absolute mt-6   "
+            />
 
-          {/* Main Logo */}
-          <Image
-            src="/logo1.png"
-            alt="logo"
-            width={300}
-            height={300}
-            className="absolute  animate-moveUpDown"
-          />
+            {/* Main Logo */}
+            <Image
+              src="/logo1.png"
+              alt="logo"
+              width={300}
+              height={300}
+              className="absolute  animate-moveUpDown"
+            />
 
-          {/* Overlay Image */}
-          <Image
-            src="/gentop-2.png"
-            alt="overlay"
-            width={524}
-            height={550}
-            className="ml-3 absolute  "
-          />
+            {/* Overlay Image */}
+            <Image
+              src="/gentop-6.png"
+              alt="overlay"
+              width={524}
+              height={550}
+              className="ml-3 absolute  "
+            />
+            {/* <div className="absolute z-10 bottom-[-50px] w-[300px] h-[20px] bg-blue-500 rounded-full"></div> */}
+          </div>
         </div>
 
         {/* <div className="text-center text-3xl md:text-7xl font-extrabold pb-10"> */}
@@ -47,6 +50,11 @@ function HeroSection() {
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. since the 1500s,
           </h2>
+        </div> */}
+        {/* <div className="relative bg-blue-500 h-64 w-full">
+          <div className="absolute top-0 w-full h-32 bg-white clip-path-custom-bottom">
+            <div className="h-full w-full bg-blue-500"></div>
+          </div>
         </div> */}
       </section>
     </div>
