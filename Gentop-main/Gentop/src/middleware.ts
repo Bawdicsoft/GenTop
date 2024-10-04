553e450e974d282d646b88a03873825f90481901
umersheikh1986 <142933742+umersheikh1986@users.noreply.github.com>
2024-10-04 15:50:44 +0500

    minor changes

--- Gentop-main/Gentop/src/middleware.ts
+++ Gentop-main/Gentop/src/middleware.ts
@@ -4,7 +4,7 @@ export function middleware(request: NextRequest) {
   const blockedCountries = ["USA"]; // Example: Block USA and Germany
   const country =
     request.geo?.country || request.headers.get("x-vercel-ip-country");
-  console.log(country, "country>>");
+  // console.log(country, "country>>");
 
   if (blockedCountries.includes(country || "")) {
     return new Response("Access Denied", { status: 403 });
